2cb7060cb486dffdca81c009683a1ec517329460
aishwarya <aishshevkar@gmail.com>
2024-12-05 16:24:52 -0600

    SPA changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,31 @@
 //Footer.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import './Styles/FooterStyles.css';
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleTerms = () => {
+    navigate("/terms");
+  };
+
+  const handleContact = () => {
+    navigate("/contact");
+  };
+
   return (
     <footer className="footer">
       <div className="footer-content">
         <p className="footer-text">© 2024 Kolhapuris. All Rights Reserved.</p>
         <div className="social-links">
-          <Link to="/terms" target="_blank" rel="noopener noreferrer" className="social-icon">Terms of Service</Link>
-          <Link to="/contact" target="_blank" rel="noopener noreferrer" className="social-icon">Contact Us</Link>
+        <div onClick={handleTerms} className="social-icon">
+            Terms of Service
+          </div>
+          <div onClick={handleContact} className="social-icon">
+            Contact Us
+          </div>
+
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">Facebook</a>
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">Instagram</a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-icon">Twitter</a>
